e9975907c6e4a7354ef5337dcc6b71191e1ae179
sergdon <sergdon76@gmail.com>
2023-03-10 10:55:03 +0300

    init

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -2,20 +2,16 @@ import java.util.Random;
 
 public class MagicBox<T> {
 
-    private int value;
+    private int size;
 
     private T[] items;
-    public MagicBox(int value) {
-        this.value = value;
-        this.items = (T[]) new Object[value];
+    public MagicBox(int size) {
+        this.size = size;
+        this.items = (T[]) new Object[size];
     }
 
     public boolean add(T item) {
-        for (T i : items) {
-            if (i == null) {
-                i = item;
-            }
-        }
+
         for (int j = 0; j < items.length; j++) {
             if (items[j] == null) {
                 items[j] = item;
@@ -41,7 +37,7 @@ public class MagicBox<T> {
 
     private int random() {
         Random random = new Random();
-        int randomInt = random.nextInt(value - 1);
+        int randomInt = random.nextInt(size - 1);
         return randomInt;
     }
 }
